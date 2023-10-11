79ed436f68d5209ad871dee960dc00269768dd76
GangLiu9999 <gang.liu@noaa.gov>
2023-10-11 20:32:02 +0000

    Revised code file from_gangliu/v2/Grid_covariates_ocean_color_dynamic.py and updated run_forec_model_v2.sh

--- from_gangliu/v2/archive_past_files/Grid_covariates_ocean_color_dynamic_asof20231010.py
+++ from_gangliu/v2/archive_past_files/Grid_covariates_ocean_color_dynamic_asof20231010.py
@@ -7,36 +7,8 @@ Last update: 2022-June-23
 
 Gang Liu
 
------------------------------------------------------------------------------------------
-Revision history:
 
-2023/10/11 Gang Liu:  1) Original indexing using oc_doy[i] is incorrect because 
-                         its value range is 1-365 while indexing for the following
-                         two statement should be 0-354. The processing on 
-                         2023/10/09 failed due to requesting values for oc_doy[i] 
-                         = 365 (as a string value) while the maximum allowed
-                         value for the following two module calls is 364. 
 
-                             median = tw_kd490_median[oc_doy[i], :, :]
-                             kd90th = tw_kd490_90th[oc_doy[i], :, :]
-
-                         As a result, the following indexing is implemented:
-
-                             index_str_oc_doy = str(int(oc_doy[i])-1)
-
-                         and the above two statements were revised to
-
-                             median = tw_kd490_median[index_str_oc_doy, :, :]
-                             kd90th = tw_kd490_90th[index_str_oc_doy, :, :]
-
-                      2) The following statement was also added to handle leap 
-                         year request for Day 366:
-
-                             if index_str_oc_doy == '365':    # For Day 366 of a year
-                                 index_str_oc_doy = '364'
-
------------------------------------------------------------------------------------------
- 
 
 """
 
@@ -60,10 +32,6 @@ oc_dates = oc_dates.Date.unique()
 oc_dates = pd.to_datetime(oc_dates, format = '%Y-%m-%d')
 oc_doy = oc_dates.strftime('%j')
 
-print('  oc_dates: ', oc_dates)
-print('  ')
-print('  oc_doy: ', oc_doy)
-
 # load ocean color data
 oc_long_term = nc.Dataset(input_path + 'long_term_metrics_20220531.nc')
 oc_pixel_id = nc.Dataset(input_path + 'reef_grid_pixel_id.nc')
@@ -82,42 +50,10 @@ tw_kd490_90th = oc_long_term.variables['three_week_kd490_90th']
 # this loop takes about 1-4 minutes on standard laptop
 oc_metrics = pd.DataFrame()
 
-print('  ')
-print('  ----- for i in range(len(oc_doy))', len(oc_doy))
-
 for i in range(len(oc_doy)):
     # subset to correct day
-
-    #------------------------------------------------------------------------------------
-    # Revision is done in the part and describe in the 2023/10/11 revision history:
-
-    # For troubleshooting: 
-    #print('  ')
-    #print('  i, oc_doy[i], oc_doy[i]-1 ',i,oc_doy[i],str(int(oc_doy[i])-1))
-    #print('  i, type(oc_doy[i]), type(oc_doy[i]-1) ',i,type(oc_doy[i]),type(str(int(oc_doy[i])-1)))
-
-    # Old (as of 2023/10/10):
-    #median = tw_kd490_median[oc_doy[i], :, :]
-    #kd90th = tw_kd490_90th[oc_doy[i], :, :]
-
-    # New (2023/10/11): 
-    index_str_oc_doy = str(int(oc_doy[i])-1)
-
-    # Use data of Day 365 for Day 366 
-    if index_str_oc_doy == '365':
-        index_str_oc_doy = '364'
-
-    print('  ')
-    print('  i, oc_doy[i], index_str_oc_doy ', i, oc_doy[i], index_str_oc_doy)
-
-    median = tw_kd490_median[index_str_oc_doy, :, :]
-    kd90th = tw_kd490_90th[index_str_oc_doy, :, :]
-
-    #------------------------------------------------------------------------------------
-
-    print('  median: ', median)
-    print('  kd90th: ', kd90th)
-
+    median = tw_kd490_median[oc_doy[i], :, :]
+    kd90th = tw_kd490_90th[oc_doy[i], :, :]
     # get values for specified pixels
     median_values = median[ids_loc[:,0], ids_loc[:,1]]
     kd90th_values = kd90th[ids_loc[:,0], ids_loc[:,1]]

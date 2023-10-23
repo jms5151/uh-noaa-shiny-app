2422aa3bbabcad76af768511ddb5ac7cb8f3e588
juniperlsimonis <dapperstats@gmail.com>
2023-10-23 10:59:04 -0700

    pin

--- R/forec-package.R
+++ R/forec-package.R
@@ -1,4 +1,17 @@
-#' @importFrom leaflet invokeMethod
+
+# @importFrom flexdashboard
+#' @importFrom leaflet addCircleMarkers addLegend addPolygons addScaleBar addTiles colorBin highlightOptions invokeMethod leaflet markerClusterOptions setView
+# @importFrom magrittr `%>%`
+# @importFrom plotly
+#' @importFrom sf st_read
+# @importFrom shiny
+# @importFrom shinyBS
+# @importFrom shinycssloaders
+# @importFrom shinydashboard
+# @importFrom shinythemes
+# @importFrom shinyWidgets
+# @importFrom tidyverse
+# @importFrom xts
 
 #' @keywords internal
 #'
---
+++ R/zzz_10_load_objects.R
@@ -0,0 +1,35 @@
+if(FALSE){
+# load all data -----------------------------------------------------------------------
+
+# list files
+filenames <- list.files("../forec_shiny_app_data/Forecasts/", full.names=TRUE)
+filenames <- c(filenames, list.files("../forec_shiny_app_data/Scenarios/", full.names=TRUE))
+filenames <- c(filenames, list.files("../forec_shiny_app_data/Static_data/", full.names=TRUE))
+
+# load rds files
+rds_filenames <- filenames[grep("historical|placeholder", filenames)]
+lapply(rds_filenames, load, .GlobalEnv)
+
+# load shapefiles
+shp_filenames <- filenames[grep(".shp", filenames)]
+shpFiles <- lapply(shp_filenames, st_read)
+names(shpFiles) <- gsub('.*\\/|\\.shp', '', shp_filenames)
+# list2env(shpFiles, envir = .GlobalEnv)
+
+# load csv files
+csv_filenames <- filenames[grep(".csv", filenames)]
+csvFiles <- lapply(csv_filenames, read.csv)
+names(csvFiles)<- gsub('.*\\/|\\.csv', '', csv_filenames)
+
+
+
+# format data
+shpFiles <- lapply(shpFiles, function(x) update_drisk(df = x))
+csvFiles <- lapply(csvFiles, function(x) update_date(df = x))
+
+# load individual dataframes from list of csv files 
+list2env(csvFiles, envir = .GlobalEnv)
+
+# fix columns names in warning levels table
+colnames(warning_levels_table) <- gsub('\\_', ' ', colnames(warning_levels_table))
+}

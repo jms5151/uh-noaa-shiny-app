cade0677e33430e157ae4c475c11643ea163922b
juniperlsimonis <dapperstats@gmail.com>
2024-02-02 18:45:05 -0800

    Update app.R

--- R/app.R
+++ R/app.R
@@ -40,7 +40,6 @@ app_global <- function (main_dir = ".") {
          value = load_ws_forecast(main_dir = main_dir),
          pos   = 1)
 
-
   assign(x     = "ga_nowcast_aggregated_to_management_zones",
          value = load_ga_nowcast_aggregated_to_management_zones(main_dir = main_dir),
          pos   = 1)
@@ -58,6 +57,13 @@ app_global <- function (main_dir = ".") {
          pos   = 1)
 
 
+  assign(x     = "basemap",
+         value = create_basemap( ),
+         pos   = 1)
+
+  assign(x     = "historicalMap",
+         value = create_historicalMap( ),
+         pos   = 1)
 }
 
 #'
@@ -70,11 +76,13 @@ app_clear_global <- function ( ) {
   rm("historical_data", pos = 1)
   rm("ga_forecast", pos = 1)
   rm("ws_forecast", pos = 1)
-
   rm("ga_nowcast_aggregated_to_management_zones", pos = 1)
   rm("ws_nowcast_aggregated_to_management_zones", pos = 1)
   rm("ga_gbr_nowcast_aggregated_to_gbrmpa_park_zones", pos = 1)
   rm("ws_gbr_nowcast_aggregated_to_gbrmpa_park_zones", pos = 1)
+
+  rm("basemap", pos = 1)
+  rm("historicalMap", pos = 1)
 
 }
 

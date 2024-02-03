66ce37e1fc60215b0573f36753d7e0b4b10e0d87
juniperlsimonis <dapperstats@gmail.com>
2024-02-02 17:06:06 -0800

    Update forec_shiny_app.R

--- codes/forec_shiny_app.R
+++ codes/forec_shiny_app.R
@@ -11,26 +11,26 @@ app_global(main_dir = "..")
 
 shinyApp(app_ui( ), app_server)
 
-#source("./forec_shiny_app_packages.R", local = TRUE)
+source("./forec_shiny_app_packages.R", local = TRUE)
 
 # load data 
-#source("./forec_shiny_app_load_objects.R", local = TRUE)
+source("./forec_shiny_app_load_objects.R", local = TRUE)
 
 # load functions, maps, plots, user interface and server for shiny app 
-#source("./forec_shiny_app_maps.R", local = TRUE)
+source("./forec_shiny_app_maps.R", local = TRUE)
 
 # load plots and plotting functions
-#source("./forec_shiny_app_plots.R", local = TRUE)
+source("./forec_shiny_app_plots.R", local = TRUE)
 
 # load information text blocks and settings, must be loaded before sourcing UI and server
-#source("./forec_shiny_app_info_text_and_settings.R", local = TRUE)
+source("./forec_shiny_app_info_text_and_settings.R", local = TRUE)
 
 # load user interface
-#source("./forec_shiny_app_user_interface.R", local = TRUE)
+source("./forec_shiny_app_user_interface.R", local = TRUE)
 
 # load server
-#source("./forec_shiny_app_server.R", local = TRUE)
+source("./forec_shiny_app_server.R", local = TRUE)
 
 # run shiny app -------------------------------------------------------------------
-#shinyOptions(cache = cachem::cache_mem(max_size = 500e6))
-#shinyApp(ui, server)
+shinyOptions(cache = cachem::cache_mem(max_size = 500e6))
+shinyApp(ui, server)

7c828ebac35ff8297474fb692da2a9a2ef878626
juniperlsimonis <dapperstats@gmail.com>
2024-02-02 16:35:33 -0800

    Update forec_shiny_app.R

--- codes/forec_shiny_app.R
+++ codes/forec_shiny_app.R
@@ -6,7 +6,7 @@ library(uhnoaashinyapp)
 uhnoaashinyapp:::read_app_text( )
 uhnoaashinyapp:::read_app_settings( )
 uhnoaashinyapp:::load_historical_data( )
-uhnoaashinyapp:::load_ga_forecast(main_dir = file.path(".", "app"))
+uhnoaashinyapp:::load_ga_forecast(main_dir = file.path("app"))
 
 source("./forec_shiny_app_packages.R", local = TRUE)
 

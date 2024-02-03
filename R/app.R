c07a211cb3e213ad3c225b14d7ee911b4aedaa5a
juniperlsimonis <dapperstats@gmail.com>
2024-02-02 19:10:02 -0800

    Update app.R

--- R/app.R
+++ R/app.R
@@ -73,38 +73,6 @@ app_global <- function (main_dir = ".") {
          value = load_shape_files(main_dir = main_dir),
          pos   = 1)
 
-  assign(x     = "ga_pac_basevals_ID",
-         value = load_ga_pac_basevals_id(main_dir = main_dir),
-         pos   = 1)
-
-  assign(x     = "ga_pac_scenarios",
-         value = load_ga_pac_scenarios(main_dir = main_dir),
-         pos   = 1)
-
-  assign(x     = "ws_pac_basevals_ID",
-         value = load_ws_pac_basevals_id(main_dir = main_dir),
-         pos   = 1)
-
-  assign(x     = "ws_pac_scenarios",
-         value = load_ws_pac_scenarios(main_dir = main_dir),
-         pos   = 1)
-
-  assign(x     = "ga_gbr_basevals_ID",
-         value = load_ga_gbr_basevals_id(main_dir = main_dir),
-         pos   = 1)
-
-  assign(x     = "ga_gbr_scenarios",
-         value = load_ga_gbr_scenarios(main_dir = main_dir),
-         pos   = 1)
-
-  assign(x     = "ws_gbr_basevals_ID",
-         value = load_ws_gbr_basevals_id(main_dir = main_dir),
-         pos   = 1)
-
-  assign(x     = "ws_gbr_scenarios",
-         value = load_ws_gbr_scenarios(main_dir = main_dir),
-         pos   = 1)
-
 }
 
 #'
@@ -127,14 +95,6 @@ app_clear_global <- function ( ) {
 
   rm("shpFiles", pos = 1)
 
-  rm("ga_pac_basevals_ID", pos = 1)
-  rm("ga_pac_scenarios", pos = 1)
-  rm("ws_pac_basevals_ID", pos = 1)
-  rm("ws_pac_scenarios", pos = 1)
-  rm("ga_gbr_basevals_ID", pos = 1)
-  rm("ga_gbr_scenarios", pos = 1)
-  rm("ws_gbr_basevals_ID", pos = 1)
-  rm("ws_gbr_scenarios", pos = 1)
 
 }
 
